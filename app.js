e1c7a5832ec480e5ac8791d140322d6f21a1ecbb
RootAndroid58 <rootand58@gmail.com>
2020-06-27 17:37:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 8080;
+
+var router = express.Router();
+
+router.get('/', function (request, response) {
+  response.render('home.ejs');
+});
+
+router.get('/services', function (request, response) {
+  response.render('services.ejs');
+});
+
+router.get('/about', function (request, response) {
+  response.render('about.ejs');
+});
+
+router.get('/news', function (request, response) {
+  response.render('news.ejs');
+});
+
+router.get('/faq', function (request, response) {
+  response.render('faq.ejs');
+});
+
+app.use('/', router);
+
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
